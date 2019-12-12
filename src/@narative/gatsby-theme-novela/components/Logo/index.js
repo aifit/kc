3f0b1a5589fc1a7af1fe6a22e75c07fafb4347bf
Yahya J Aifit <yahya@yja.me>
2019-12-12 22:44:18 +0700

    add logo

---
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+/**
+ * Paste in your SVG logo and return it from this component.
+ * Make sure you have a height set for your logo.
+ * It is recommended to keep the height within 25-35px.
+ */
+export default function Logo() {
+  return (
+    <svg
+      viewBox="0 0 93 20"
+      height="20px"
+      x="0px"
+      y="0px"
+      width="93px"
+    >
+      <image
+        id="image0"
+        width="93"
+        height="20"
+        x="0"
+        y="0"
+        xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAF0AAAAUCAQAAABFL/nsAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+  AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElN
+  RQfjDAwSJggBhe09AAADCklEQVRIx9XW/VHjSBAF8J+pDUBkoI0AEQHjCDARICJYHMD6KwGbCGwi
+  sDcCeyM4XQRoI1hdBNwfGoSEbdZbtUXdPZVLntF095vXPR+9rz4Qiep3TWZHv5wJnpsnYNtpHUeQ
+  /gaDgbVnPz179mT7J3Q4U5goIb4fbGj1HkZm6/bkKEtrwY1zl1bSX4hyMvXKNJKs3zupUr/pPYzT
+  aTOXo2+jUnj4E7Th0wGFUv1OTQa3Unyziv0DOVG773bI3MpQebTp+MvcY6WI7aL1LTFwLWmsBi6k
+  Sg++CHi0gtEY/D3bNP9/zFZvqY8F/Y77pVxhI5gb6ysE6zilgInCulUCA3d1wIgv4Fur51ISiW9l
+  FsgNTEwj4dK9QioVXLnDQIadTRN1Z3XWIZ6bWHSIz+UqfVN9pcQaOz0w0dMzVSndOdfzWdmQfVUd
+  nSwWdmAtszA0tMBEom+FVF/fZxvko5zZ5avxrB+tnXWCzJFHTWpd7rGJgWu3A/u4i6VUWjVku9SL
+  PZtMaLLxvRlZtkYPwbUjaBfMWimTWrrphC1jqw5w8aaWX5DKXBzdO/Z39OtWhOygTRktj6CteuHS
+  DoNG2SunIdh6sj6415dxYocxt7U1f8f3SdTv4o95NPjnJOKZraBwHmu1i+Jd6n295tkdHFGdQr3y
+  Uq1pXGpFZ951+B97PurUD48Eqau5nb9gLImyHJvSa9ziFOo1pmAixU5JU79XqFqVnjgFKyXyVj0v
+  TRo/48bLvrcBxCPsgCxnEiGa1e/SAvXWVa/y+lAJBm+0zY0t5TEPc8FYfoD8jQrLKMFYqmzipP4y
+  Nrb1s0U+RTDH3awEO4RRPgqjFz96X0PrMjQxReIpuulhYB7TWho2mi8jycLUxtx9DPAQj6teh3za
+  mVThMcrzYkdhaIexSTOuNLSpb46j1DayWKjqMb2TLr2ZRPWm6jKJstk4U2lsBRQH6z5EktVe76uf
+  mvrla7zXS+8okyhmFaOA4jTqH4eaeitn793X/7f4b1G/jytie3C5v8GnXw/5QFQHjrSj+BclqQCn
+  mqharAAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAxOS0xMi0xMlQxODozODowOCswMzowMKmkO/8AAAAl
+  dEVYdGRhdGU6bW9kaWZ5ADIwMTktMTItMTJUMTg6Mzg6MDgrMDM6MDDY+YNDAAAAGXRFWHRTb2Z0
+  d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAABJRU5ErkJggg==" />
+    </svg>
+  );
+}
